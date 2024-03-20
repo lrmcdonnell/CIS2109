58d7b4930eeff8faf02bcbb98266d3d522d962b8
Laura McDonnell <129304200+lrmcdonnell@users.noreply.github.com>
2024-03-20 12:50:59 -0400

    Add files via upload

---
+++ Lecture Notes/Week7.sql
@@ -0,0 +1,15 @@
+-- ER diagrams: entity-relationship diagrams
+
+-- Entity types: nouns 
+-- relationships: describe connections between nouns
+
+-- Unary relationship: 1 entity has a relationship with itself
+-- Binary relationship: 2 entities have a relationship
+-- Ternary relationship: multiple entities with a relationship
+
+-- Associative entity: relationships can have attributes
+    -- these describe features pertaining to the association between entities in the relationship
+
+-- Associative entity - combination of relationship and entity
+    -- many attributes on a relationship suugest use of an entity instead of a relationship
+    -- has to be many-to-many
